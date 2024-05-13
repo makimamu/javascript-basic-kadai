48fd6fc3d5ff4ab5d299017f65ee00b6edbd2c81
makimamu <156180587+makimamu@users.noreply.github.com>
2024-05-14 01:58:50 +0900

    taipingのアップ

---
+++ taiping/main.js
@@ -0,0 +1,17 @@
+// 機能分解し（担当する空の関数）を用意
+
+// ランダムなテキストを表示 
+const createText = () => {};
+
+//キー入力の表示
+const keyPress = e => {};
+
+// タイピンスキルのランクを判定
+const rankCheck = score => {};
+
+// ゲーム終了
+const gameOver = Id => {};
+
+// カウントダウンタイマー
+const timer = () => {}; 
+ 
